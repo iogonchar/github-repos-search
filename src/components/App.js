9ab1d601923fa3503bd0d9401b62e056f9aee365
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-09-16 16:11:45 +0300

    feat: layout for the Card component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import SearchBar from "./SearchBar";
+import Card from "./Card";
 
 const App = () => {
   return (
     <div>
       <SearchBar />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <ul className="cards">
+      <li className="card">
+        <h2 className="card__title">ant-design/ant-design-mobile</h2>
+        <p className="card__description">A configurable mobile UI</p>
+        <p className="card__main-lang">TypeScript</p>
+        <p className="card__stars">9.2k</p>
+      </li>
+    </ul>
+  );
+}
+
+export default Card;
