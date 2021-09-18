48fe8d09d0bd8b3c7f30570a5ce64ec87fb2f47e
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-09-18 19:01:35 +0300

    style: Repository component styled

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -3,20 +3,31 @@ import React from "react";
 const Repository = (props) => {
   console.log('rep state', props)
   return (
-    <div>
-      <p></p>
-      <p>{ props.location.state.node.nameWithOwner }</p>
-      <p>{ props.location.state.node.description }</p>
-      <p>{ props.location.state.node.primaryLanguage.name }</p>
-      <p>Stars: { props.location.state.node.stargazerCount }</p>
-      <p>Issues: { props.location.state.node.issues.totalCount }</p>
-      <p>Pull Requests: { props.location.state.node.pullRequests.totalCount }</p>
-
-        {
-          props.location.state.node.languages.nodes.map((item) => <p key={item.color}>{item.name} {item.color}</p>)
-        }
-
-    </div>
+    <section className="content">
+      <h1 className="content__title">Информация о репозитории</h1>
+      <ul className="repository">
+        <li className="repository__item">{ props.location.state.node.nameWithOwner }</li>
+        <li className="repository__item">{ props.location.state.node.description }</li>
+        <li className="repository__item">
+          {
+            props.location.state.node.languages.nodes.length > 0
+              ? props.location.state.node.languages.nodes.map((item) =>
+                  <p className="repository__lang" key={item.color}>
+                    <span className="repository__lang-color" style={{backgroundColor: item.color}}></span>
+                    { item.name }
+                  </p>
+                )
+              : 'Язык репозитория не указан.'
+          }
+        </li>
+        <li className="repository__item">Stars: { props.location.state.node.stargazerCount }</li>
+        <li className="repository__item">Issues: { props.location.state.node.issues.totalCount }</li>
+        <li className="repository__item">Pull Requests: { props.location.state.node.pullRequests.totalCount }</li>
+          {/* {
+            props.location.state.node.languages.nodes.map((item) => <li key={item.color}>{item.name} {item.color}</li>)
+          } */}
+      </ul>
+    </section>
   );
 }
 
